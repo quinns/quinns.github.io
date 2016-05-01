848b7a1712406fb2c2f1d02d326e8f9e2d5a7875
Quinn Supplee <quinn@quinnsupplee.com>
2016-05-01 09:04:53 -0700

    viewport adjustment for android

--- js/fetch-lolcat.js
+++ js/fetch-lolcat.js
@@ -9,7 +9,6 @@ jQuery(function($) {
 	fetch('https://api.giphy.com/v1/gifs/random?tag=funny+cat&rating=pg&api_key=dc6zaTOxFJmzC&limit=1').then(function(response) { 
 		return response.json();
 	}).then(function(my_result) {
-		console.log(my_result);
 		$('#fetch-result').html('<img src="'+my_result.data.fixed_height_downsampled_url+'" class="img-responsive center-block">');
 	});
 });
