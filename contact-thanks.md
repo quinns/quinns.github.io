8e6ef9c3ce71671afff4b5dda8819571bfd02bd2
Quinn Supplee <quinn@quinnsupplee.com>
2016-10-16 19:32:58 -0700

    added new contact form

---
+++ contact-thanks.md
@@ -0,0 +1,14 @@
+---
+layout: page
+title: Thank you!
+permalink: /contact-thanks/
+custom_js:
+- fetch
+- fetch-thanks
+---
+
+<p>Your message has been sent.</p>
+
+<div id="fetch-result" style="margin-bottom: 2em;"></div>
+
+<p><a href="/contact" class="btn btn-default">Back to form</a></p>
---
+++ js/fetch-thanks.js
@@ -0,0 +1,15 @@
+/*
+fetch an LOLCAT GIF from Giphy API
+parse and render it using Javascript/jQuery
+
+adapted from: https://davidwalsh.name/fetch and http://www.mkyong.com/javascript/how-to-access-json-object-in-javascript/
+*/
+
+jQuery(function($) {
+	fetch('https://api.giphy.com/v1/gifs/random?tag=thanks&rating=pg&api_key=dc6zaTOxFJmzC&limit=1').then(function(response) { 
+		return response.json();
+	}).then(function(my_result) {
+		$('#fetch-result').html('<img src="'+my_result.data.fixed_height_downsampled_url+'" class="img-responsive">');
+	});
+});
+
